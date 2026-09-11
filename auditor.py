24aee969e12d36cf425f4f00dc8a122f844f5728
Foo Shi Bin <2501668@sit.singaporetech.edu.sg>
2026-09-11 15:15:34 +0800

    edited auditor

--- auditor.py
+++ auditor.py
@@ -3,8 +3,7 @@ failed_entries = 0
 
 print("SMART INVENTORY AUDITOR")
 print("Type 'quit' to exit\n")
-#colon is required to start the block of code, indentation is required to indicate the block of code belongs to the if statement, while loop, or function definition.
-##single equal is to assign the value to the variable, double equal is to compare the value of the variable """
+
 while True:
     user_input = input ("Enter Stock Quantity: ")
 
